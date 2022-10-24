84799dad149b55ee82c8dda1836aa0df872c4897
hilucasalves <hilucasalves@ufmg.br>
2022-10-24 15:35:21 -0300

    Commit Inicial

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  width: 100%;
+  background-color: lightgrey;
+`;
+
+const StyledList = styled.ul`
+  /*background-color: ${({ theme }) => theme.colors.primary}*/ ;
+`;
+
+export async function getStaticProps() {
+  const products = await fetch('http://localhost:3002/products').then(resp =>
+    resp.json()
+  );
+  return {
+    props: {
+      products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default function Home({ products }) {
+  return (
+    <Container>
+      <StyledList>
+        {products.map(product => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </StyledList>
+    </Container>
+  );
+}
